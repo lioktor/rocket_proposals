e5deffd35dccdd9797d3fc3b8f2329412e64b937
flavie sicard <flavie.sicard@hotmail.com>
2019-06-04 14:20:52 +0200

    nw routes to start

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :users do
+    resources :opportunities, only: [:new, :create, :index]
+  end
+  resources :clients
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
