53781a300492bbe585096919b17f505cbe07e911
flavie sicard <flavie.sicard@hotmail.com>
2019-06-05 18:04:33 +0200

    new client, new opportunities, my opportunities + views associées

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,22 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: 'pages#home'
-  resources :users do
-    resources :client, only: [:new]
-  end
+  resources :users
+
   resources :clients do
-    resources :opportunities
+    resources :opportunities  do
+      collection do
+        get 'myopportunities'
+      end
+    end
   end
-  resources :quotations
-  # resources :opportunities, only: [:index, :new, :show]
+
   resources :missions do
     resources :trainings, only: [:index, :new, :show]
   end
+
+  resources :opportunities, only: [:index] do
+      resources :quotations, only: [:index, :new, :show]
+    end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
