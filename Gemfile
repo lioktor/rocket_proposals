9d5063fc311c54d95a6162fa1ea5d8f0a84dacd4
Ktorza <lionel.ktorza@gmail.com>
2019-06-05 10:17:33 +0200

    initialization of cloudinary and added the mission reference in the quotation table

--- Gemfile
+++ Gemfile
@@ -27,4 +27,5 @@ group :development, :test do
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
   gem 'dotenv-rails'
+  gem 'cloudinary', '~> 1.9.1'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_04_115516) do
+ActiveRecord::Schema.define(version: 2019_06_05_081247) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,6 +84,8 @@ ActiveRecord::Schema.define(version: 2019_06_04_115516) do
     t.bigint "opportunity_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "mission_id"
+    t.index ["mission_id"], name: "index_quotations_on_mission_id"
     t.index ["opportunity_id"], name: "index_quotations_on_opportunity_id"
   end
 
@@ -136,6 +138,7 @@ ActiveRecord::Schema.define(version: 2019_06_04_115516) do
   add_foreign_key "clients", "users"
   add_foreign_key "opportunities", "clients"
   add_foreign_key "opportunities", "users"
+  add_foreign_key "quotations", "missions"
   add_foreign_key "quotations", "opportunities"
   add_foreign_key "users", "users", column: "manager_id"
 end
