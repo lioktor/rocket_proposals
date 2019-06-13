856f32f2a21ce08849e16e0d1966f5db4ac366f2
Ktorza <lionel.ktorza@gmail.com>
2019-06-13 11:58:15 +0200

    dashboard done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,7 @@ import "../plugins/sidebar";
 
 
 
+
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
@@ -11,3 +12,4 @@ import { initAutocomplete } from '../plugins/init_autocomplete';
 
 initMapbox();
 initAutocomplete();
+
