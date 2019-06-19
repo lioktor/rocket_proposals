e1ec67b4c86cdfac5d07674498fa7b5966b4f0b5
flavie sicard <flavie.sicard@hotmail.com>
2019-06-19 09:53:56 +0200

    remove bug when sign in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home] # activation de cette ligne suite à création landing page
+  skip_before_action :authenticate_user!#, only: [:home] # activation de cette ligne suite à création landing page
   layout 'home_layout', only: [:home]
 
   def home
